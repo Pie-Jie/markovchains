b7c817b6c1a5c04582bda4fd5a9dfa1c9f29e37e
Pie-Jie <pie.jie@hotmail.com>
2017-11-10 00:56:54 +0100

    Made it to a trump rap

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
-const test = express();
-const end = express();
 const Twit = require('twit');
-
-const Markov = require('markov-strings');
 
 const data = [];
 
-var tweet = '';
-var fakeText;
+var tweet;
+var rap;
 
 var T = new Twit({
     consumer_key: 'Zh89x9BRtSBeBwYrvHtsNxdEv',
@@ -21,52 +17,39 @@ var T = new Twit({
 
 var stream = T.stream('statuses/filter', {
     language: 'en',
-    track: '#trump'
+    track: 'trump'
 });
 
 stream.on('message', function (msg) {
     data.push(msg.text);
     tweet = msg.text;
-    //var Text = require('markov-chains-text').default;
-    //fakeText = new Text(tweet);
-    //console.log(fakeText)
-    if(data.length >= 10){
+
+    if (data.length >= 10) {
         stream.stop();
     }
 });
-/*
-var freestyle = require('freestyle');
-//var fs = require('fs');
-
-//var s = fs.createReadStream(tweet);
-
-freestyle(tweet, function (r) {
-    var A = r.couplet();
-    var B = r.couplet();
-    
-    console.log([
-        A[0], B[0], A[1], B[1], ''
-    ].join('\n'));
-});
-
-*/
-
-
-var twaddle = require('twaddle');
- 
-var bullshit = twaddle.generate('politics-us-trump-j-donald', {paragraphs: 5}) ;
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
-    
-    res.send('<h1>Fake Tweets for Donald</h1><h2>' + tweet + '</h2><p>' + fakeText.rejoinedText + '</p><a href="">Refresh</a>');   
-    
-    
-    
+
 });
 
+var freestyle = require('freestyle');
+
+app.get('/gettrumprap', function (req, res) {
+    
+    freestyle(tweet, function (r) {
+        var A = r.couplet();
+        var B = r.couplet();
+
+        rap = [A[0], B[0], A[1], B[1], ''].join('\n');
+    });
+    
+   res.send('<blockquote>' + rap + '</blockquote>');
+});
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function () {
     console.log("Listening on " + port);
 });
