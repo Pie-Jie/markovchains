8a54e28777b8777ebb5f367801a85e992d895727
Pie-Jie <pie.jie@hotmail.com>
2017-11-09 23:55:38 +0100

    Updated files to recent version

--- index.js
+++ index.js
@@ -1,55 +1,72 @@
 const express = require("express");
 const app = express();
+const test = express();
+const end = express();
+const Twit = require('twit');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const Markov = require('markov-strings');
 
+const data = [];
 
- var Twit = require('twit');
+var tweet = '';
+var fakeText;
 
 var T = new Twit({
-  consumer_key:         'Zh89x9BRtSBeBwYrvHtsNxdEv',
-  consumer_secret:      'cmCfoDrOp67u7a8bsY0sB6fk0Aayhql41ZrVNFBEtqgK4FYEHx',
-  access_token:         '42051552-prICERMck8rcpPKYG38fcSHggHuQ2vijI8Ro4mmDf',
-  access_token_secret:  'mQmlRdOAirzXiBG2SHf8hwUSA25t2KHBVUN58K1Klongm',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    consumer_key: 'Zh89x9BRtSBeBwYrvHtsNxdEv',
+    consumer_secret: 'cmCfoDrOp67u7a8bsY0sB6fk0Aayhql41ZrVNFBEtqgK4FYEHx',
+    access_token: '42051552-prICERMck8rcpPKYG38fcSHggHuQ2vijI8Ro4mmDf',
+    access_token_secret: 'mQmlRdOAirzXiBG2SHf8hwUSA25t2KHBVUN58K1Klongm',
+    timeout_ms: 60 * 1000,
 });
 
-//
-//  filter the twitter public stream by the word 'mango'.
-//
-var stream = T.stream('user', { track: 'trump', user_name: 'PieJieD' });
+var stream = T.stream('statuses/filter', {
+    language: 'en',
+    track: '#trump'
+});
 
 stream.on('message', function (msg) {
-    //console.log(msg.text);
+    data.push(msg.text);
+    tweet = msg.text;
+    //var Text = require('markov-chains-text').default;
+    //fakeText = new Text(tweet);
+    //console.log(fakeText)
+    if(data.length >= 10){
+        stream.stop();
+    }
 });
-
-var port = process.env.PORT || 8888;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-    
-});
-//
-// Twit has promise support; you can use the callback API,
-// promise API, or both at the same time.
-//
 /*
-T.get('account/verify_credentials', { skip_status: true })
-  .catch(function (err) {
-    console.log('caught error', err.stack)
-  })
-  .then(function (result) {
-    // `result` is an Object with keys "data" and "resp".
-    // `data` and `resp` are the same objects as the ones passed
-    // to the callback.
-    // See https://github.com/ttezel/twit#tgetpath-params-callback
-    // for details.
+var freestyle = require('freestyle');
+//var fs = require('fs');
 
-    console.log('data', result.data);
-  })
+//var s = fs.createReadStream(tweet);
+
+freestyle(tweet, function (r) {
+    var A = r.couplet();
+    var B = r.couplet();
+    
+    console.log([
+        A[0], B[0], A[1], B[1], ''
+    ].join('\n'));
+});
 
 */
 
 
+var twaddle = require('twaddle');
+ 
+var bullshit = twaddle.generate('politics-us-trump-j-donald', {paragraphs: 5}) ;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+    
+    res.send('<h1>Fake Tweets for Donald</h1><h2>' + tweet + '</h2><p>' + fakeText.rejoinedText + '</p><a href="">Refresh</a>');   
+    
+    
+    
+});
 
 
-
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("Listening on " + port);
+});
